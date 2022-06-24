53551c68debbcafcf9328227ec4533f65b6b04cf
vtrtga <vitor.ypvalim@gmail.com>
2022-06-24 12:50:21 -0300

    req5

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,19 +5,47 @@ import PropTypes from 'prop-types';
 class Form extends Component {
   render() {
     const { currencies } = this.props;
+    const paymentMethods = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    const category = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
     return (
       <form>
         <input
-          placeholder="Valor"
-          name="value-input"
-          data-testid="value-input"
+          placeholder="Descrição"
+          name="description"
+          data-testid="description-input"
+          type="text"
         />
-
+        <input
+          placeholder="Valor"
+          name="value"
+          data-testid="value-input"
+          type="number"
+        />
+        <label htmlFor="paymentMethod">
+          <select data-testid="method-input">
+            {
+              paymentMethods.map((method) => (
+                <option key={ method } value={ method }>{ method }</option>
+              ))
+            }
+          </select>
+        </label>
         <label htmlFor="moeda">
-          <select name="moeda">
+          Moeda
+          <select id="moeda" name="moeda">
             {
               currencies.map((key) => (
                 <option key={ key } value={ key }>{ key }</option>
+              ))
+            }
+          </select>
+        </label>
+        <label htmlFor="category">
+          Categoria
+          <select data-testid="tag-input">
+            {
+              category.map((cat) => (
+                <option key={ cat } value={ cat }>{ cat }</option>
               ))
             }
           </select>
